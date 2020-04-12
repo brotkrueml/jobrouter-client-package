57d5fb474745d4814ba4c7be342489f87655e30e
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-04-12 15:59:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Packaging the JobRouter Client
+
+Script for packaging the current stable [JobRouter Client](https://github.com/brotkrueml/jobrouter-client)
+release.
+
+    ./package.sh
+    
+The generated files (zip, tar.gz and accompanying hash files) can be found in the `archive` folder -
+ready for upload.
---
+++ package.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# This script packages a release for the JobRouter Client
+#
+
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ARCHIVE_DIR=archive
+
+VERSION=$(grep -Po "(?<=private const VERSION = ')([0-9]+\\.[0-9]+\\.[0-9]+)" \
+  $PROJECT_DIR/vendor/brotkrueml/jobrouter-client/src/Information/Version.php)
+
+TAR_FILENAME=jobrouter-client-$VERSION.tar.gz
+ZIP_FILENAME=jobrouter-client-$VERSION.zip
+INFO_TEMPLATE_FILENAME=jobrouter-client-template.txt
+INFO_FILENAME=jobrouter-client.txt
+
+echo "Version of JobRouter Client: $VERSION"
+
+echo "Removing old folders and files ..."
+rm -r "$PROJECT_DIR/vendor"
+rm "$PROJECT_DIR/composer.lock"
+rm "$PROJECT_DIR/$INFO_FILENAME"
+
+echo "Install composer packages ..."
+composer install --prefer-dist --no-dev --no-progress --no-suggest --working-dir="$PROJECT_DIR"
+
+echo "Writing info file ..."
+cp "$PROJECT_DIR/$INFO_TEMPLATE_FILENAME" "$PROJECT_DIR/$INFO_FILENAME"
+sed -i -- "s/##VERSION##/$VERSION/g" "$PROJECT_DIR/$INFO_FILENAME"
+
+echo "Creating archives ..."
+(cd "$PROJECT_DIR" && mkdir -p "$ARCHIVE_DIR")
+(cd "$PROJECT_DIR" && tar acf "$ARCHIVE_DIR/$TAR_FILENAME" vendor "$INFO_FILENAME")
+(cd "$PROJECT_DIR" && zip -r "$ARCHIVE_DIR/$ZIP_FILENAME" vendor "$INFO_FILENAME")
+
+echo "Generating hash files ..."
+(cd "$PROJECT_DIR/$ARCHIVE_DIR" && sha256sum "$TAR_FILENAME" > "$TAR_FILENAME.sha256.txt")
+(cd "$PROJECT_DIR/$ARCHIVE_DIR" && sha256sum "$ZIP_FILENAME" > "$ZIP_FILENAME.sha256.txt")
